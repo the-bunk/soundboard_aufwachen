356cb0e30823d874c99a02ca10f75169107a1e05
Alex <you@example.com>
2018-12-16 00:46:48 +0100

    für mod_admin vorbereitet

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_session import Session
-from app.core import db
+from app.core import db, security
 
 import app.models as models_sounds
 from .site import mod_site
+import app.admin.models_security as models_security
+from .admin import mod_admin
 
 
 app = Flask(__name__,  static_folder='static')
@@ -14,11 +16,11 @@ Session(app)
 db.init_app(app)
 db.create_all(app=app)  # app=app weil sonst applicationbound error
 
+security.init_app(app, models_security.user_datastore)
+
 app.register_blueprint(mod_site)
+app.register_blueprint(mod_admin, url_prefix='/admin')
 
 app.jinja_env.globals.update(config=app.config)
-
-def init_db():
-    pass
 
 # seed_security()
--- app/core.py
+++ app/core.py
@@ -4,5 +4,8 @@ app.core
 core module
 """
 from flask_sqlalchemy import SQLAlchemy
+from flask_security import Security
 
 db = SQLAlchemy()
+
+security = Security()
