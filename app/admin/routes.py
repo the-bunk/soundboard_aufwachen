670b7d60932b30cd1d8441d6de8bd423fd481303
Alex <you@example.com>
2018-12-17 18:25:35 +0100

    so jetzt hier fröhlich weiter develhoppen

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 import datetime
-from flask import Blueprint, render_template, g
+import os
+from flask import Blueprint, render_template, request, current_app, flash
 from flask_security import login_required, roles_accepted, current_user
-from app.models import Board, Sound
+from werkzeug.utils import secure_filename
 from app import db
+from app.core import remove_html
+from app.models import Board, Sound
 from .models_security import User, Role, user_datastore
 
 mod_admin = Blueprint('admin', __name__, template_folder='templates', static_folder='static/admin')
@@ -21,7 +24,7 @@ def init_my_blueprint():
         if not user_datastore.get_user(u[0]):
             user_datastore.create_user(email=u[0],
                                        confirmed_at=datetime.datetime.now(),
-                                       password='')
+                                       password='test1234')
             db.session.commit()
             for r in u[2]:
                 user_datastore.add_role_to_user(u[0], r)
@@ -34,7 +37,9 @@ def init_my_blueprint():
 @login_required
 @roles_accepted('admin')
 def admin():
-    return render_template('admin/admin.html')
+    roles = Role.query.all()
+    users = User.query.all()
+    return render_template('admin/admin.html', users=users, roles=roles)
 
 
 @mod_admin.route('/users')
@@ -48,11 +53,62 @@ def admin_users():
 @login_required
 @roles_accepted('admin')
 def admin_boards():
-    return render_template('admin/boards.html')
+    sounds = Sound.query.all()
+    boards = Board.query.all()
+    return render_template('admin/boards.html', sounds=sounds, boards=boards)
 
 
 @mod_admin.route('/sounds')
 @login_required
 @roles_accepted('admin')
 def admin_sounds():
-    return render_template('admin/sounds.html')
+    sounds = Sound.query.all()
+    return render_template('admin/sounds.html', sounds=sounds)
+
+
+@mod_admin.route('/sound/submit', methods=["POST"])
+def sound_submit():
+    name = remove_html(request.form['name'])
+    description = remove_html(request.form['description'])
+    soundfile = request.files['soundfile']
+
+    if Sound.query.filter_by(name=name).first():
+        flash('Dieser Name existiert bereits.', 'danger')
+        return "1"
+
+    # save file
+    filename = secure_filename(soundfile.filename)
+    db_filename = "sounds/" + filename
+    filename = os.path.join(current_app.config['SOUNDS_DIR'], filename)
+    soundfile.save(filename)
+
+    # add to database
+    new_sound = Sound(name=name, description=description, soundfile=db_filename, enabled=True)
+    db.session.add(new_sound)
+    db.session.commit()
+
+    flash('Soundfile übertragen, dankeschön.', 'success')
+    return "0"
+
+
+@mod_admin.route('/board/submit', methods=["POST"])
+def board_submit():
+    name = remove_html(request.form['name'])
+    sounds = "1, 2,3 "  #TODO
+
+    if Board.query.filter_by(name=name).first():
+        flash('Dieser Name existiert bereits.', 'danger')
+        return "1"
+
+    # add to database
+    new_board = Board(name=name)
+    db.session.add(new_board)
+    db.session.commit()
+    for s in sounds:
+        sound = Sound.query.filter_by(id=s).first()
+        if sound:
+            new_board.sounds.append(sound)
+            db.session.commit()
+
+    flash('Board erstellt.', 'success')
+    return "0"
--- app/admin/static/admin/js/custom.js
+++ app/admin/static/admin/js/custom.js
@@ -6,46 +6,82 @@ file.onchange = function(){
     }
 };
 
-function addItem() {
-    document.getElementById("bt_submit").className = "button is-pulled-right is-info is-loading";
-    var name = document.getElementById("tb_name").value;
-    var description = document.getElementById("tb_description").value;
-    var soundfile = document.getElementById("soundfile");
 
-    // check data
-    if (!isStr(name)) {
-        alert("error: name");
-        return;
+function submitSound() {
+  document.getElementById("bt_submit").className =
+    "button is-pulled-right is-info is-loading"
+  var name = document.getElementById("tb_name").value
+  var description = document.getElementById("tb_description").value
+  var soundfile = document.getElementById("soundfile")
+
+  // if (!isStr(name)) {
+  //     alert("error: name");
+  //     return;
+  // }
+  // if (!isStr(description)) {
+  //     alert("error: description");
+  //     return;
+  // }
+
+  var formData = new FormData()
+  formData.append("name", name)
+  formData.append("description", description)
+  formData.append("soundfile", soundfile.files[0])
+
+  var route = "/admin/sound/submit"
+
+  var target = "main_content"
+  // KRÜCKE
+  var request = new XMLHttpRequest()
+  request.open("POST", route)
+  request.ontimeout = function() {
+    window.location.href = "/admin/sounds"
+  }
+  request.onload = function() {
+    if (request.readyState === 4) {
+      if (request.status === 200) {
+        window.location.href = "/admin/sounds"
+      }
     }
-    if (!isStr(description)) {
-        alert("error: description");
-        return;
-    }
-
-    var formData = new FormData();
-    formData.append("name", name);
-    formData.append("description", description);
-    formData.append("soundfile", soundfile.files[0]);
-
-    var route = "/admin/sound/submit";
-
-
-
-    //TODO
-    var target = "main_content";
-    // KRÜCKE
-    var request = new XMLHttpRequest();
-    request.open("POST", route);
-    request.ontimeout = function() {
-        window.location.href = subdir_kochbuch + "/create_item";
-    };
-    request.onload = function() {
-        if (request.readyState === 4) {
-            if (request.status === 200) {
-                window.location.href = subdir_kochbuch + "/create_item";
-            }
-        }
-    };
-    request.timeout = 10000;
-    request.send(formData);
+  }
+  request.timeout = 10000
+  request.send(formData)
 }
+
+
+function toggleSoundToBoard(sound_id){
+    var e = document.getElementById('sound_'+sound_id)
+    if (e.classList.contains('is-danger')){
+        e.classList.remove('is-danger')
+        e.classList.add('is-success')
+    }
+    else{
+        e.classList.remove('is-success')
+        e.classList.add('is-danger')
+    }
+}
+
+
+function getSoundsToBoard(){
+    var sound_list = [];
+    var x = document.getElementById("sounds_to_board");
+    var y = x.getElementsByTagName("a");
+    for (let item of y) {
+        if (item.classList.contains('is-success')){
+            sound_list.push(item.getAttribute('data-id'));
+        }
+    }
+    return sound_list;
+}
+
+
+function submitBoard(){
+    var name = document.getElementById('tb_name');
+    var sounds = getSoundsToBoard();
+    alert(sounds);
+    
+
+    //TODO jetzt noch dieses doofe xhtml-request zeug
+}
+
+
